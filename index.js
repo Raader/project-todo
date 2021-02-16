b907e35908101b4dedb4561373f6541dbe5a4c31
Efe <48628713+Raader@users.noreply.github.com>
2021-02-16 18:01:40 +0300

    setup

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const config = require("config");
 
+//create express app
+const app = express();
 app.get("/",(req,res) => {
     res.send("hello");
 });
 
+//use routes for all routes besides the main route
+const routes = require("./routes/routes");
+app.use(routes);
+
+//connect to mongoDB database
+const dbURI = config.get("dbURI");
+mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true});
+const db = mongoose.connection;
+//catch connection errors and log activity
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once("open",function(){
+    console.log("connected to the database");
+})
+
+//start the server
 const port = process.env.PORT || 5000;
 app.listen(port,() => console.log("server running at port: " + port))
--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 const expect = require("chai").expect;
 const request = require("request");
+const config = require("config");
 
 describe("Main route",function(){
-    const url = "http://localhost:5000"
+    const url = config.get("host");
     it("responds to get main request",function(done){
         request(url,function(error,res,body){
             expect(res.statusCode).to.equal(200);
