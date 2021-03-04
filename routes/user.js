745b7a1f73e2b701041d35eca9cbe555fafb9744
Efe <48628713+Raader@users.noreply.github.com>
2021-03-04 14:59:34 +0300

    fix err msg sending on backend

--- routes/user.js
+++ routes/user.js
@@ -6,19 +6,19 @@ const router = express.Router();
 
 router.post("/login", (req, res) => {
     controller.login(req, res).catch((err) => {
-        res.status(400).json({ err });
+        res.status(400).json({ err: err.message });
     });
 });
 
 router.post("/register", (req, res) => {
     controller.register(req, res).catch((err) => {
-        res.status(400).json({ err });
+        res.status(400).json({ err: err.message });
     });
 });
 
 router.get("/", auth, (req, res) => {
     controller.get(req, res).catch((err) => {
-        res.status(400).json({ err });
+        res.status(400).json({ err: err.message });
     });
 });
 module.exports = router;
