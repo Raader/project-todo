84fb640e379c505e113de3547e7f72dc4340dfe2
Efe <48628713+Raader@users.noreply.github.com>
2021-04-11 01:39:39 +0300

    project cont style

--- client/src/components/ProjectSection.js
+++ client/src/components/ProjectSection.js
@@ -18,70 +18,72 @@ export function ProjectSection(props) {
   return (
     <div className="project-main">
       <Section>
-        <div className="project-cont">
-          <h3 className="project-name">
-            <i class="fas fa-bars"></i> {project.name}
-          </h3>
-          <div className="project-stats">
-            <Container>
-              <Row>
-                <Col className="no-padding">
-                  <div className="project-date">
-                    <i class="fas fa-hourglass-start"></i> Last Edited:{" "}
-                    <span style={{ fontWeight: 600 }}>20/05/21</span>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col className="no-padding">
-                  <div className="project-date">
-                    <i class="fas fa-hourglass-end"></i> Last Completed:{" "}
-                    <span style={{ fontWeight: 600 }}>20/05/21</span>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col className="no-padding">
-                  <div className="project-date">
-                    <i class="fas fa-calendar-week"></i> Created:{" "}
-                    <span style={{ fontWeight: 600 }}>
-                      {new Date(project.created).toLocaleDateString("en-US", {
-                        year: "numeric",
-                        month: "2-digit",
-                        day: "2-digit",
-                      })}
-                    </span>
-                  </div>
-                </Col>
-              </Row>
-              <Row className="project-todos">
-                <Col>
-                  <div id="completed" className="project-stat">
-                    5 <i class="fas fa-check-square"></i>
-                    <br></br>completed
-                  </div>
-                </Col>
-                <Col>
-                  <div id="remaining" className="project-stat">
-                    5 <i class="far fa-check-square"></i>
-                    <br></br>remaining
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </div>
-          <div className="project-footer">
-            <ButtonGroup vertical>
-              <Button variant="nice" onClick={() => history.push("/select")}>
-                <i class="fas fa-exchange-alt"></i> Change Project
-              </Button>
-              <Button variant="nice">
-                <i class="fas fa-edit"></i> Edit Project
-              </Button>
-              <Button variant="nice" id="delete-btn">
-                <i class="fas fa-trash"></i> Delete Project
-              </Button>
-            </ButtonGroup>
+        <div>
+          <div className="project-cont">
+            <h3 className="project-name">
+              <i class="fas fa-bars"></i> {project.name}
+            </h3>
+            <div className="project-stats">
+              <Container>
+                <Row>
+                  <Col className="no-padding">
+                    <div className="project-date">
+                      <i class="fas fa-hourglass-start"></i> Last Edited:{" "}
+                      <span style={{ fontWeight: 600 }}>20/05/21</span>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="no-padding">
+                    <div className="project-date">
+                      <i class="fas fa-hourglass-end"></i> Last Completed:{" "}
+                      <span style={{ fontWeight: 600 }}>20/05/21</span>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="no-padding">
+                    <div className="project-date">
+                      <i class="fas fa-calendar-week"></i> Created:{" "}
+                      <span style={{ fontWeight: 600 }}>
+                        {new Date(project.created).toLocaleDateString("en-US", {
+                          year: "numeric",
+                          month: "2-digit",
+                          day: "2-digit",
+                        })}
+                      </span>
+                    </div>
+                  </Col>
+                </Row>
+                <Row className="project-todos">
+                  <Col>
+                    <div id="completed" className="project-stat">
+                      5 <i class="fas fa-check-square"></i>
+                      <br></br>completed
+                    </div>
+                  </Col>
+                  <Col>
+                    <div id="remaining" className="project-stat">
+                      5 <i class="far fa-check-square"></i>
+                      <br></br>remaining
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+            <div className="project-footer">
+              <ButtonGroup vertical>
+                <Button variant="nice" onClick={() => history.push("/select")}>
+                  <i class="fas fa-exchange-alt"></i> Change Project
+                </Button>
+                <Button variant="nice">
+                  <i class="fas fa-edit"></i> Edit Project
+                </Button>
+                <Button variant="nice" id="delete-btn">
+                  <i class="fas fa-trash"></i> Delete Project
+                </Button>
+              </ButtonGroup>
+            </div>
           </div>
         </div>
       </Section>
