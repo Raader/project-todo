41eb7fffc0fe81d4740e9fdd50d41ecd328dda01
Efe <48628713+Raader@users.noreply.github.com>
2021-04-22 16:53:45 +0300

    heroku configs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+const path = require("path");
 
 //create express app
 const app = express();
@@ -27,6 +28,15 @@ db.once("open", function () {
     console.log("connected to the database");
 });
 
+//serve client
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 //start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("server running at port: " + port));
